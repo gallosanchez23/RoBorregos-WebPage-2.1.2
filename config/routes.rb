09ce0f9ac785bcb33ebffb5e4881ae97bac8b410
Eduardo Sanchez <gallo.sanchez23@gmail.com>
2017-03-27 21:38:35 -0600

    Adds new private namespace and public resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :public do
+    collection do
+      get 'members'
+    end
+  end
+
+  namespace :private, module: 'private' do
+    root 'users#index'
+
+    devise_for :users, :controllers => { :sessions => 'private/sessions' }
+
+    resources :users do
+      collection do
+        get :active_users
+        get :inactive_users
+      end
+    end
+  end
+
 end
